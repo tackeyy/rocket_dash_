ac0d7fc7270285f8558003b5d520b72354c22409
Yusuke Takita <takitayy@gmail.com>
2018-02-08 17:40:32 +0900

    Admin CRUD (#2)

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/', to: 'dashboard#index'
     resource :sign_in, controller: 'sign_in', only: %i[show create]
+
+    resources :admins
   end
 
   resource :users, only: [] do
