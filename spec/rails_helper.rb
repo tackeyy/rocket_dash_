2ae4f3431dfc8b6abbfb8f7e5fa6182e6ec5e3fb
Yusuke Takita <takitayy@gmail.com>
2018-02-14 22:26:35 +0900

    Rspec (#6)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,9 +3,13 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each do |file|
+  require file
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
