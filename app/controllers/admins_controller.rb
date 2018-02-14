c83391c7753f4b36366827d1ee6e06db5e62197e
tackeyy <takitayy@gmail.com>
2018-02-14 21:51:09 +0900

    Fix haml-lint offenses

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -10,6 +10,7 @@ class AdminsController < ActionController::Base
   def current_admin
     @current_admin ||= Admin.find_by(id: session[:admin_id])
   end
+  helper_method :current_admin
 
   private
 
