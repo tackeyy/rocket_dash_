df86c5d17301c8cae279496f4136b640cd7b8932
tackeyy <takitayy@gmail.com>
2018-06-28 20:57:09 +0900

    Goodbye sorcery

--- db/schema.rb
+++ db/schema.rb
@@ -21,28 +21,4 @@ ActiveRecord::Schema.define(version: 2018_01_23_090208) do
     t.datetime "deleted_at"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "type"
-    t.string "crypted_password"
-    t.string "salt"
-    t.string "remember_me_token"
-    t.datetime "remember_me_token_expires_at"
-    t.string "reset_password_token"
-    t.datetime "reset_password_token_expires_at"
-    t.datetime "reset_password_email_sent_at"
-    t.string "activation_state"
-    t.string "activation_token"
-    t.datetime "activation_token_expires_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
-    t.index ["activation_token"], name: "index_users_on_activation_token"
-    t.index ["deleted_at"], name: "index_users_on_deleted_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
-    t.index ["type"], name: "index_users_on_type"
-  end
-
 end
